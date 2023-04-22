b069c35d09e66228f86252997e33f0687091197e
System Administrator <root@Karans-MacBook-Pro.local>
2023-04-22 09:48:47 +0530

    DSA Coding Saga 3.0

---
+++ 01. Coding Saga [ DSA 3.0 ]/DSA 3.0/01. Getting Started/01. helloWorld.java
@@ -0,0 +1,10 @@
+import java.util. *;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.print("Hello World, Welcome to DSA 3.0");
+        System.out.println("Road To DSA Begons");
+    }
+}
+
+
---
+++ 01. Coding Saga [ DSA 3.0 ]/DSA 3.0/01. Getting Started/03. dataTypes.java
@@ -0,0 +1,10 @@
+import java.util. *;
+
+public class Main {
+    public static void main(String[] args) {
+        int marks = 95;
+        double percentage = 93.4;
+        System.out.println("marks is " + marks);
+        System.out.println("percentage is " + percentage);
+    }
+}
